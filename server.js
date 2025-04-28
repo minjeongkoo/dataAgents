724ad8dabf624f960e0612d4e4b5413df9232ae5
minjeongkoo <koominjeong99@gmail.com>
2025-04-28 14:26:15 +0900

    log 삭제

--- server.js
+++ server.js
@@ -72,7 +72,6 @@ function parseCompactFormat(buffer) {
   const numberOfBeamsPerScan = buffer.readUInt32LE(offset); offset += 4;
   const numberOfEchosPerBeam = buffer.readUInt32LE(offset); offset += 4;
 
-  console.log(`[UDP] Message from ${rinfo.address}:${rinfo.port} (${msg.length} bytes)`);
   console.log(`[DEBUG] Header Info:`);
   console.log(`  numberOfLinesInModule: ${numberOfLinesInModule}`);
   console.log(`  numberOfBeamsPerScan: ${numberOfBeamsPerScan}`);
